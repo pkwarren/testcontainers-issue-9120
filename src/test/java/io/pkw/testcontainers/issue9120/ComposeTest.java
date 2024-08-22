2405e65bdb3a42f134a7aaf36b4830be72542bdd
Philip K. Warren <pkwarren@users.noreply.github.com>
2024-08-22 11:02:27 -0500

    Fixes to constructor

--- src/test/java/io/pkw/testcontainers/issue9120/ComposeTest.java
+++ src/test/java/io/pkw/testcontainers/issue9120/ComposeTest.java
@@ -5,14 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
 
 @Testcontainers
 @Execution(ExecutionMode.CONCURRENT)
 public class ComposeTest {
     @Container
-    public static ComposeContainer COMPOSE_CONTAINER = new ComposeContainer("docker-compose.yml");
+    public static ComposeContainer COMPOSE_CONTAINER = new ComposeContainer(new File("docker-compose.yml"))
+            .waitingFor("kafka", Wait.forLogMessage(".*Transitioning from RECOVERY to RUNNING.*", 1));
 
     @Test
     void testCompose() {
